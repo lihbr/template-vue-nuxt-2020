a39a6ec880d70520ea1ebe4e4c37df0ab33ceeb1
lihbr <lihbr@users.noreply.github.com>
2018-12-16 14:56:54 +0100

    :lipstick: remove default style link

--- components/controls/SmartLink.vue
+++ components/controls/SmartLink.vue
@@ -42,15 +42,3 @@ export default {
   }
 };
 </script>
-
-<style scoped lang="stylus">
-.container
-  display block
-  overflow hidden
-  padding 0 8vw
-
-  @media screen and (max-width: desktop)
-    padding 0 50px
-  @media screen and (max-width: tablet)
-    padding 0 20px
-</style>
