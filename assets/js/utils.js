68f14b933eff1b441e8db2ede0c33e81c96ade21
lihbr <lihbr@users.noreply.github.com>
2019-09-07 18:47:06 +0200

    :wrench: change throttle function

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -38,21 +38,25 @@ const debounce = function(func, wait, immediate) {
  * @param {number} rate - rate
  * @return {function}
  */
-const throttle = function(func, rate) {
-  var lastTime = new Date(),
-    api;
-  rate = rate || 1000;
-
-  api = function() {
-    var context = this,
-      args = arguments;
-    var now = new Date();
-    if (now - lastTime >= rate) {
+const throttle = (func, limit) => {
+  let lastFunc;
+  let lastRan;
+  return function() {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
       func.apply(context, args);
-      lastTime = now;
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function() {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
     }
   };
-  return api;
 };
 
 /**
