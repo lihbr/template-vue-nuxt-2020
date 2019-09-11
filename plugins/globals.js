295b3df06bb52ab2a6d04b61a32925e46d7dcefa
lihbr <lihbr@users.noreply.github.com>
2019-09-11 20:01:05 +0200

    :fire: remove container

--- plugins/globals.js
+++ plugins/globals.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 
-import Container from "~/components/layout/Container.vue";
 import SmartLink from "~/components/controls/SmartLink.vue";
 
-Vue.component("Container", Container);
 Vue.component("SmartLink", SmartLink);
