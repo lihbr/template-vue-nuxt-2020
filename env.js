90e7722b56bf3446a4172b9e7e5ee075c5b86149
lihbr <lihbr@users.noreply.github.com>
2019-12-08 21:08:21 +0100

    refactor: turn env into an async function

--- env.js
+++ env.js
@@ -1,34 +1,38 @@
 const pkg = require("./package");
 require("dotenv").config();
 
-const DEV = process.env.NODE_ENV === "development";
-const COMMIT_REF = process.env.COMMIT_REF
-  ? process.env.COMMIT_REF
-  : "unknow commit reference";
+const config = async () => {
+  const DEV = process.env.NODE_ENV === "development";
+  const COMMIT_REF = process.env.COMMIT_REF
+    ? process.env.COMMIT_REF
+    : "unknow commit reference";
 
-const APP_NAME = process.env.APP_NAME || pkg.name;
-const APP_DESC = process.env.APP_DESC || pkg.description;
-const APP_HOST = process.env.APP_HOST || "0.0.0.0";
-const APP_PORT = process.env.APP_PORT || 3000;
-const APP_URL = process.env.APP_URL || `http://${APP_HOST}:${APP_PORT}`;
-const APP_LANG = process.env.APP_LANG || "en";
+  const APP_NAME = process.env.APP_NAME || pkg.name;
+  const APP_DESC = process.env.APP_DESC || pkg.description;
+  const APP_HOST = process.env.APP_HOST || "0.0.0.0";
+  const APP_PORT = process.env.APP_PORT || 3000;
+  const APP_URL = process.env.APP_URL || `http://${APP_HOST}:${APP_PORT}`;
+  const APP_LANG = process.env.APP_LANG || "en";
 
-const GTM_ID = process.env.GTM_ID || "";
-const GTM_FRIENDLY = process.env.GTM_FRIENDLY || false;
-const GTM_DEV = process.env.GTM_DEV || false;
+  const GTM_ID = process.env.GTM_ID || "";
+  const GTM_FRIENDLY = process.env.GTM_FRIENDLY || false;
+  const GTM_DEV = process.env.GTM_DEV || false;
 
-module.exports = {
-  DEV,
-  COMMIT_REF,
+  return {
+    DEV,
+    COMMIT_REF,
 
-  APP_NAME,
-  APP_DESC,
-  APP_HOST,
-  APP_PORT,
-  APP_URL,
-  APP_LANG,
+    APP_NAME,
+    APP_DESC,
+    APP_HOST,
+    APP_PORT,
+    APP_URL,
+    APP_LANG,
 
-  GTM_ID,
-  GTM_FRIENDLY,
-  GTM_DEV
+    GTM_ID,
+    GTM_FRIENDLY,
+    GTM_DEV
+  };
 };
+
+module.exports = { config };
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,149 +1,147 @@
-const env = require("./env");
+module.exports = async () => {
+  const env = await require("./env").config();
 
-module.exports = {
-  /*
-   ** Application mode
-   */
-  mode: "universal",
+  const script = [];
 
-  /*
-   ** Application directory
-   */
-  srcDir: "src/",
+  return {
+    /*
+     ** Application mode
+     */
+    mode: "universal",
 
-  /*
-   ** Headers of the page, see in ./config
-   */
-  head: {
-    title: env.APP_NAME,
-    htmlAttrs: {
-      lang: env.APP_LANG,
-      class: ""
+    /*
+     ** Application directory
+     */
+    srcDir: "src/",
+
+    /*
+     ** Headers of the page, see in ./config
+     */
+    head: {
+      title: env.APP_NAME,
+      titleTemplate: `%s - ${env.APP_NAME}`,
+      htmlAttrs: {
+        lang: env.APP_LANG,
+        class: ""
+      },
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, user-scalable=no"
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: env.APP_DESC
+        },
+        { name: "msapplication-TileColor", content: "#da532c" },
+        { name: "theme-color", content: "#feeeee" }
+      ],
+      script,
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png"
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png"
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png"
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest"
+        },
+        {
+          rel: "mask-icon",
+          href: "/safari-pinned-tab.svg",
+          color: "#e84311"
+        }
+      ]
     },
-    meta: [
-      { charset: "utf-8" },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1, user-scalable=no"
-      },
-      {
-        hid: "description",
-        name: "description",
-        content: env.APP_DESC
-      },
-      { name: "msapplication-TileColor", content: "#da532c" },
-      { name: "theme-color", content: "#feeeee" }
+
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: "#000000" },
+
+    /*
+     ** Global CSS
+     */
+    css: ["~/assets/sass/style.sass"],
+
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [{ src: "~/plugins/welcome", mode: "client" }],
+
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [],
+
+    /*
+     ** Nuxt.js build modules
+     */
+    buildModules: [
+      "@nuxtjs/eslint-module",
+      "~/modules/build",
+      "~/modules/generate",
+      "~/modules/components",
+      "~/modules/tailwindcss",
+      "~/modules/netlify-cms",
+      "@nuxtjs/style-resources",
+      "@nuxtjs/sitemap",
+      ["@nuxtjs/netlify-files", { existingFilesDirectory: __dirname }],
+      [
+        "@nuxtjs/google-tag-manager",
+        {
+          id: env.GTM_ID,
+          pageTracking: true,
+          respectDoNotTrack: env.GTM_FRIENDLY,
+          dev: env.GTM_DEV
+        }
+      ],
+      "nuxt-svg-loader"
     ],
-    script: [],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        href: "/apple-touch-icon.png"
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: "/favicon-32x32.png"
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        href: "/favicon-16x16.png"
-      },
-      {
-        rel: "manifest",
-        href: "/site.webmanifest"
-      },
-      {
-        rel: "mask-icon",
-        href: "/safari-pinned-tab.svg",
-        color: "#e84311"
-      }
-    ]
-  },
 
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: "#000000" },
+    /*
+     ** Style resources
+     */
+    styleResources: {
+      sass: "~/assets/sass/core.sass"
+    },
 
-  /*
-   ** Global CSS
-   */
-  css: ["~/assets/sass/style.sass"],
+    /*
+     ** Sitemap
+     */
+    sitemap: {
+      hostname: env.APP_URL,
+      gzip: true,
+      exclude: ["/_admin/**", "/_adminAssets/**", "/_adminBundle/**"]
+    },
 
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [{ src: "~/plugins/welcome", mode: "client" }],
+    /*
+     ** Env
+     */
+    env,
 
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [],
-
-  /*
-   ** Nuxt.js build modules
-   */
-  buildModules: [
-    "@nuxtjs/eslint-module",
-    "~/modules/build",
-    "~/modules/generate",
-    "~/modules/components",
-    "~/modules/tailwindcss",
-    "~/modules/netlify-cms",
-    "@nuxtjs/style-resources",
-    "@nuxtjs/sitemap",
-    ["@nuxtjs/netlify-files", { existingFilesDirectory: __dirname }],
-    [
-      "@nuxtjs/google-tag-manager",
-      {
-        id: env.GTM_ID,
-        pageTracking: true,
-        respectDoNotTrack: env.GTM_FRIENDLY,
-        dev: env.GTM_DEV
-      }
-    ],
-    "nuxt-svg-loader"
-  ],
-
-  /*
-   ** Style resources
-   */
-  styleResources: {
-    sass: "~/assets/sass/core.sass"
-  },
-
-  /*
-   ** Sitemap
-   */
-  sitemap: {
-    hostname: env.APP_URL,
-    gzip: true,
-    exclude: ["/_admin/**", "/_adminAssets/**", "/_adminBundle/**"]
-  },
-
-  /*
-   ** Env
-   */
-  env,
-
-  /*
-   ** Server configuration
-   */
-  server: {
-    host: env.APP_HOST,
-    port: env.APP_PORT
-  },
-
-  /*
-   ** Render
-   */
-  render: {
-    ssr: !env.DEV
-  }
+    /*
+     ** Server configuration
+     */
+    server: {
+      host: env.APP_HOST,
+      port: env.APP_PORT
+    }
+  };
 };
