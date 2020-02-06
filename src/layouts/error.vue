5d140933b63c96691c6ad4dd922916d12be74fc3
lihbr <lihbr@users.noreply.github.com>
2020-02-06 10:40:57 +0100

    feat: cache generation for page payload

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <h1>{{ code }}</h1>
-    <h2>{{ error.message }}</h2>
+    <h2>{{ message }}</h2>
     <smart-link class="underline" to="/">
       Home page
     </smart-link>
@@ -9,19 +9,24 @@
 </template>
 
 <script>
+const unknown = "Something happened, but we're taking care of it!";
+
 export default {
   props: {
     error: {
       type: Object,
       default: () => ({
         code: "Unknown",
-        message: "Something happened, but we're taking care of it!"
+        message: ""
       })
     }
   },
   computed: {
     code: function() {
       return this.error.statusCode || this.error.code;
+    },
+    message() {
+      return this.error.msg /*|| statusMsg[this.code]*/ || unknown;
     }
   }
 };
--- src/middleware/pagePayload.js
+++ src/middleware/pagePayload.js
@@ -1,13 +1,36 @@
-export default context => {
+import fetch from "isomorphic-unfetch";
+
+import logger from "~/assets/js/logger";
+
+export default async context => {
   if (context.payload) {
     context.pagePayload = { data: context.payload };
   } else {
-    const content = process.env.APP_ROUTES.find(
-      i => i.route === context.route.fullPath
-    );
-    if (!content) {
-      return context.error({ statusCode: 404, message: "Page not found!" });
+    try {
+      // Resolve cache url
+      const url = [
+        process.env.GENERATE_CACHE_DIR,
+        context.route.fullPath,
+        "index.json"
+      ]
+        .join("/")
+        .replace(/[\/\\]{3,}/g, "/");
+
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw response; // non-200 code
+      }
+      const data = await response.json();
+      context.pagePayload = { data };
+    } catch (error) {
+      logger.error(error);
+      if (!!error.statusCode) {
+        return context.error({ statusCode: error.statusCode });
+      } else if (!!error.status) {
+        return context.error({ statusCode: error.status });
+      } else {
+        return context.error();
+      }
     }
-    context.pagePayload = { data: content.payload };
   }
 };
