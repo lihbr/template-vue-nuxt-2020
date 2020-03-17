9644db436523843cff39145e3feeeb54517a8f3a
lihbr <lihbr@users.noreply.github.com>
2020-03-17 21:41:20 +0100

    feat: standalone statistics module

---
+++ src/modules/statistics/index.js
@@ -0,0 +1,37 @@
+const logger = require("./logger");
+
+const statisticsFunctions = {
+  generate() {
+    let startTime;
+    let count;
+    this.nuxt.hook("generate:routeCreated", () => {
+      if (!startTime) {
+        startTime = Date.now();
+        count = 0;
+      } else {
+        count++;
+      }
+    });
+    this.nuxt.hook("generate:done", () => {
+      const time = (Date.now() - startTime) / 1000;
+      const rps = count / time;
+      logger.info(`Generated ${count} routes in ${time} sec (${rps} r/s)`);
+    });
+  }
+};
+
+module.exports = function(moduleOptions) {
+  const options = {
+    ignore: [],
+    ...moduleOptions
+  };
+
+  // Run each non-ignored statistics functions
+  for (const key in statisticsFunctions) {
+    if (!options.ignore.includes(key)) {
+      statisticsFunctions[key].call(this);
+    }
+  }
+
+  logger.success("Statistics module initialized\n");
+};
