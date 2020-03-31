e378d05428837b370766e86ea3b61e505a36d1e2
lihbr <lihbr@users.noreply.github.com>
2020-03-31 15:14:01 +0200

    docs: document build module

--- src/modules/build/index.js
+++ src/modules/build/index.js
@@ -2,11 +2,19 @@ const logger = require("./logger");
 
 module.exports = function(moduleOptions) {
   this.nuxt.hook("build:before", () => {
+    // Extract css from HTML
     this.options.build.extractCSS = true;
 
+    // Use dart-sass
     this.options.build.loaders.sass = this.options.build.loaders.sass || {};
     this.options.build.loaders.sass.implementation = require("sass");
     this.options.build.loaders.sass.fiber = require("fibers");
+
+    // Disabling redundant minification
+    this.options.build.html = this.options.build.html || {};
+    this.options.build.html.minify = this.options.build.html.minify || {};
+    this.options.build.html.minify.minifyCSS = false;
+    this.options.build.html.minify.minifyJS = false;
   });
 
   logger.success("Build module initialized\n");
