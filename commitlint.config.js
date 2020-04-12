2288b426e04d3e4a4eeed815d3fd1cd4323bf583
lihbr <lihbr@users.noreply.github.com>
2020-04-12 22:32:17 +0200

    chore(config): update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   parserPreset: "conventional-changelog-conventionalcommits",
-  extends: ["@commitlint/config-conventional"]
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      ["deps", "functions", "datalayer", "modules", "config", "core"]
+    ],
+    "scope-empty": [2, "never"]
+  }
 };
