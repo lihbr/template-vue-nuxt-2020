4118067af485f30316d0659cf7f3bbb8a463199c
lihbr <lihbr@users.noreply.github.com>
2020-04-25 14:37:46 +0200

    feat(core): add currentColor tailwind utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,7 @@ module.exports = {
       const finalColors = {
         none: "transparent",
         transparent: "transparent",
+        current: "currentColor",
         inherit: "inherit"
       };
 
