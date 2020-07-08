7345103a7e420e3c03e2e53c075ec1d023eac756
lihbr <lihbr@users.noreply.github.com>
2020-07-08 18:57:07 +0200

    chore(misc): clean unused conf

--- src/modules/build/index.js
+++ src/modules/build/index.js
@@ -2,9 +2,6 @@ const logger = require("./logger");
 
 module.exports = function (moduleOptions) {
   this.nuxt.hook("build:before", () => {
-    // Extract css from HTML
-    this.options.build.extractCSS = !this.options.env.DEV;
-
     // Disabling redundant minification
     this.options.build.html = this.options.build.html || {};
     this.options.build.html.minify = this.options.build.html.minify || {};
